0c8a7e29685f4d3e38f6b50f24879b75584698d8
amin-amani <didi1364@gmail.com>
2024-03-16 01:13:17 +0330

    Update main.c

--- Blinky/Core/Src/main.c
+++ Blinky/Core/Src/main.c
@@ -109,7 +109,7 @@ int main(void)
   while (1)
   {
 
-	  LD2_GPIO_Port->ODR^=LD2_Pin;
+	  LD5_GPIO_Port->ODR^=LD5_Pin;
 	  HAL_Delay(100);
     /* USER CODE END WHILE */
     MX_USB_HOST_Process();
